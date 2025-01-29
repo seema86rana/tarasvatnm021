8e6a0d15cc07809600051fe12e5040d8e529e9af
seema86rana <seema86rana@gmail.com>
2025-01-29 09:32:38 +0530

    Update RouteServiceProvider.php
    change redirection name

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/bird';
+    public const HOME = '/birdview';
 
     /**
      * The controller namespace for the application.
